fdb59b1cf32a9643b71831569c4763a82d11b45e
jonathanaelies <190857950+jonathanaelies@users.noreply.github.com>
2025-06-13 13:59:52 +0200

    fixe erreur

--- src/main.c
+++ src/main.c
@@ -33,11 +33,7 @@ int main(int argc, char **argv) {
 
   }
 
-  if ( strncmp( configuration.command, "dimension", 9 ) == 0 ) {
-    /* helloworld() function is defined in feature.h and implemented in feature.c */
-    dimension();
 
-  }
   if ( strncmp( configuration.command, "dimension", 9 ) == 0 ) {
     /* helloworld() function is defined in feature.h and implemented in feature.c */
     dimension(configuration.filenames[0]);
