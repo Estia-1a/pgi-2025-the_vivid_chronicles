372842312a1254bf533156b5d9c150d2633f2a48
arthurpgt <arthur.piguet@etu.estia.fr>
2025-06-18 11:28:39 +0200

    rotate_cw

--- src/features.h
+++ src/features.h
@@ -17,4 +17,5 @@ void color_bleu(const char *input_file);
 void color_gris(const char *input_file);
 void color_invert(char *source_path);
 void color_gray_luminance(char *source_path);
+void rotate_cw(char *source_path);
 #endif
--- src/main.c
+++ src/main.c
@@ -92,5 +92,7 @@ if (strncmp(configuration.command, "color_red", 9) == 0) {
 if (strcmp(configuration.command, "color_gray_luminance")==0){
     color_gray_luminance(configuration.filenames[0]);
   }
-
+if (strcmp(configuration.command, "rotate_cw")==0){
+    rotate_cw(configuration.filenames[0]);
+  }
 }
